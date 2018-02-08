bd40c518aa3f66443af8d23d3005288e7e4ebd5b
logan.oge <logan.oge@my.tccd.edu>
2018-02-07 21:41:17 -0600

    section 2

---
+++ Classwork/Section2/Nile.Windows/MainForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nile.Windows
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad(e);
+
+            var product = new Product();
+
+            var name = product.GetName();
+
+            //product.Name = "Product A";
+            product.setName("Product A");
+            //product.Description = "None";
+            var error = product.Validate();
+
+            var str = product.ToString();
+
+            var productB = new Product();
+            //productB.Name = "Product B";
+            //productB.setName("Product B");
+            //productB.Description = product.Description;
+            error = productB.Validate();
+            
+            
+        }
+    }
+}
