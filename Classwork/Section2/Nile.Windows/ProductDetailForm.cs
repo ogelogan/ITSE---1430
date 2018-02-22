0b243dc314dcc97624bc5a255c4955d06abb880d
logan.oge <logan.oge@my.tccd.edu>
2018-02-21 21:45:47 -0600

    section 2 part 5

--- Classwork/Section2/Nile.Windows/ProductDetailForm.cs
+++ Classwork/Section2/Nile.Windows/ProductDetailForm.cs
@@ -12,23 +12,45 @@ namespace Nile.Windows
         {
             InitializeComponent();
         }
+        public ProductDetailForm(string title) : this()
+        {
+            //InitializeComponent();
+
+            Text = title;
+        }
+
+        public ProductDetailForm( Product product ) :this("Edit Product")
+        {
+            //InitializeComponent();
+
+            //Text = "Edit Product";
+            Product = product;
+        }
         #endregion
 
         /// <summary>Gets or sets the product being edited.</summary>
         public Product Product { get; set; }
+
+        //public abstract DialogResult ShowDialogEx();
+
+        //public virtual DialogResult ShowDialogEx ()
+        //{
+        //    return ShowDialog();
+        //}
 
         protected override void OnLoad( EventArgs e )
         {
             base.OnLoad(e);
 
             //load product
-            if (Product != null)
+            if (this.Product != null)
             {
                 _txtName.Text = Product.Name;
                 _txtDescription.Text = Product.Description;
                 _txtPrice.Text = Product.Price.ToString();
                 _chkIsDiscontinued.Checked = Product.IsDiscontinued;
             }
+            ValidateChildren();
         }
 
         #region Event Handlers
@@ -40,12 +62,23 @@ namespace Nile.Windows
 
         private void OnSave( object sender, EventArgs e )
         {
+            if (!ValidateChildren())
+                return;
             // Create product
             var product = new Product();
             product.Name = _txtName.Text;
             product.Description = _txtDescription.Text;
             product.Price = ConvertToPrice(_txtPrice);
             product.IsDiscontinued = _chkIsDiscontinued.Checked;
+
+
+            //Validate
+            var message = product.Validate();
+            if (!String.IsNullOrEmpty(message))
+            {     
+                DisplayError(message);
+                return;
+            }; 
 
             //Return from form
             Product = product;
@@ -55,6 +88,11 @@ namespace Nile.Windows
         }
         #endregion
         
+        private void DisplayError ( string message )
+        {
+            MessageBox.Show(this, message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
         private decimal ConvertToPrice ( TextBox control )
         {
             if (Decimal.TryParse(control.Text, out var price))
@@ -63,9 +101,29 @@ namespace Nile.Windows
             return -1;
         }
 
-        private void ProductDetailForm_Load( object sender, EventArgs e )
+        private void _txtPrice_Validating( object sender, System.ComponentModel.CancelEventArgs e )
         {
+            var textbox = sender as TextBox;
 
+            var price = ConvertToPrice(textbox);
+            if (price < 0)
+            {
+                _errorProvider.SetError(textbox, "Price is required");
+                e.Cancel = true;
+            } else
+                _errorProvider.SetError(textbox, "");
+        }
+
+        private void _txtName_Validating( object sender, System.ComponentModel.CancelEventArgs e )
+        {
+            var textbox = sender as TextBox;
+
+            if (String.IsNullOrEmpty(textbox.Text))
+            {
+                _errorProvider.SetError(textbox, "Name is required");
+                e.Cancel = true;
+            } else
+                _errorProvider.SetError(textbox, "");
         }
     }
 }
