22d85eb2c193fe2941263e1d475f03943426ad0b
logan.oge <logan.oge@my.tccd.edu>
2018-03-05 21:36:49 -0600

    section 3v2 part 1

---
+++ Classwork/Section3v2/Nile.Windows/ProductDetailForm.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Nile.Windows
+{
+    /// <summary>Provides a form for adding/editing <see cref="Product"/>.</summary>
+    public partial class ProductDetailForm : Form
+    {
+        #region Construction
+
+        public ProductDetailForm()
+        {
+            InitializeComponent();
+        }
+        public ProductDetailForm(string title) : this()
+        {
+            //InitializeComponent();
+
+            Text = title;
+        }
+
+        public ProductDetailForm( Product product ) :this("Edit Product")
+        {
+            //InitializeComponent();
+
+            //Text = "Edit Product";
+            Product = product;
+        }
+        #endregion
+
+        /// <summary>Gets or sets the product being edited.</summary>
+        public Product Product { get; set; }
+
+        //public abstract DialogResult ShowDialogEx();
+
+        //public virtual DialogResult ShowDialogEx ()
+        //{
+        //    return ShowDialog();
+        //}
+
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad(e);
+
+            //load product
+            if (this.Product != null)
+            {
+                _txtName.Text = Product.Name;
+                _txtDescription.Text = Product.Description;
+                _txtPrice.Text = Product.Price.ToString();
+                _chkIsDiscontinued.Checked = Product.IsDiscontinued;
+            }
+            ValidateChildren();
+        }
+
+        #region Event Handlers
+
+        private void OnCancel( object sender, EventArgs e )
+        {
+            //Don't need this method as DialogResult set on button
+        }
+
+        private void OnSave( object sender, EventArgs e )
+        {
+            if (!ValidateChildren())
+                return;
+            // Create product - using object initializer syntax
+            var product = new Product() {
+            Name = _txtName.Text,
+            Description = _txtDescription.Text,
+            Price = ConvertToPrice(_txtPrice),
+            IsDiscontinued = _chkIsDiscontinued.Checked,
+        };
+
+            //Validate
+            var message = product.Validate();
+            if (!String.IsNullOrEmpty(message))
+            {     
+                DisplayError(message);
+                return;
+            }; 
+
+            //Return from form
+            Product = product;
+            DialogResult = DialogResult.OK;
+            //DialogResult = DialogResult.None;
+            Close();
+        }
+        #endregion
+        
+        private void DisplayError ( string message )
+        {
+            MessageBox.Show(this, message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private decimal ConvertToPrice ( TextBox control )
+        {
+            if (Decimal.TryParse(control.Text, out var price))
+                return price;
+
+            return -1;
+        }
+
+        private void _txtPrice_Validating( object sender, System.ComponentModel.CancelEventArgs e )
+        {
+            var textbox = sender as TextBox;
+
+            var price = ConvertToPrice(textbox);
+            if (price < 0)
+            {
+                _errorProvider.SetError(textbox, "Price is required");
+                e.Cancel = true;
+            } else
+                _errorProvider.SetError(textbox, "");
+        }
+
+        private void _txtName_Validating( object sender, System.ComponentModel.CancelEventArgs e )
+        {
+            var textbox = sender as TextBox;
+
+            if (String.IsNullOrEmpty(textbox.Text))
+            {
+                _errorProvider.SetError(textbox, "Name is required");
+                e.Cancel = true;
+            } else
+                _errorProvider.SetError(textbox, "");
+        }
+    }
+}
---
+++ Classwork/Section3v2/Nile/Product.cs
@@ -0,0 +1,110 @@
+﻿/*
+ * ITSE 1430
+ * Classwork
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nile
+{
+    /// <summary>Provides information about a product.</summary>
+    public class Product : IValidatableObject
+    {
+        /// <summary>Provides information about a product.</summary>
+        public int Id { get; set; }
+
+        internal decimal DiscountPercentage = 0.10M;
+
+        /// <summary>Gets or sets the description.</summary>
+        public string Description
+        {
+            get { return _description ?? ""; }
+            set { _description = value ?? ""; }
+        }
+
+        /// <summary>Gets or sets the name.</summary>
+        /// <value></value>
+        public string Name
+        {
+            get { return _name ?? ""; }
+            set { _name = value; }
+        }
+
+        //Using auto property here
+        /// <summary>Gets or sets the price.</summary>
+        public decimal Price { get; set; } = 0;
+        //public decimal Price
+        //{
+        //    get { return _price; }
+        //    set { _price = value; }
+        //}
+
+        //public int ShowingOffAccessibility
+        //{
+        //    get { }
+        //    internal set { }
+        //}
+
+        /// <summary>Gets the price, with any discontinued discounts.</summary>
+        public decimal ActualPrice
+        {
+            get 
+            {
+                if (IsDiscontinued)
+                    return Price - (Price * DiscountPercentage);
+
+                return Price;
+            }
+            
+            //set { }
+        }
+
+        /// <summary>Determines if the product is discontinued.</summary>
+        public bool IsDiscontinued { get; set; }
+        //public bool IsDiscontinued
+        //{
+        //    get { return _isDiscontinued; }
+        //    set { _isDiscontinued = value; }
+        //}
+
+        ///// <summary>Get the product name.</summary>
+        ///// <returns>The name.</returns>
+        //public string GetName ()
+        //{
+        //    return _name ?? "";
+        //}
+
+        ///// <summary>Sets the product name.</summary>
+        ///// <param name="value">The name.</param>
+        //public void SetName ( string value )
+        //{
+        //    _name = value ?? "";
+        //}
+
+        public IEnumerable<ValidationResult> Validate( ValidationContext validationContext )
+        {
+            var errors = new List<ValidationResult>();
+
+            //Name is required
+            if (String.IsNullOrEmpty(_name))
+                errors.Add(new ValidationResult("Name cannot be empty", new[] { "Name" }));
+
+            //Price >= 0
+            if (Price < 0)
+                errors.Add(new ValidationResult("Price cannot be empty", new[] { "Price" }));
+
+            return errors;
+        }
+
+        private string _name;
+        private string _description;
+
+        //Using auto properties
+        //private decimal _price;
+        //private bool _isDiscontinued;
+    }
+}
