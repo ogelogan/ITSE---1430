22d6f1034f510109ea7cee7d96cf5888728416f7
logan.oge <logan.oge@my.tccd.edu>
2018-03-21 21:31:10 -0500

    section 4 part 1

---
+++ Classwork/Section4/Nile/Data/ProductDatabase.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nile.Data
+{
+    /// <summary>Provides an in-memory product database.</summary>
+    public abstract class ProductDatabase : IProductDatabase
+    {
+        /// <summary>Add a new product.</summary>
+        /// <param name="product">The product to add.</param>
+        /// <param name="message">Error message.</param>
+        /// <returns>The added product.</returns>
+        /// <remarks>
+        /// Returns an error if product is null, invalid or if a product
+        /// with the same name already exists.
+        /// </remarks>
+        public Product Add ( Product product, out string message )
+        {
+            //Check for null
+            if (product == null)
+            {
+                message = "Product cannot be null.";
+                return null;
+            };
+
+            //Validate product using IValidatableObject
+            var errors = product.Validate();
+            //var errors = ObjectValidator.Validate(product);
+
+            var error = Enumerable.FirstOrDefault(errors);
+            if (error != null)
+            {
+                message = error.ErrorMessage;
+                return null;
+            }
+
+            //Verify unique product
+            var existing = GetProductByNameCore(product.Name);
+            if (existing != null)
+            {
+                message = "Product already exists.";
+                return null;
+            }
+            message = null;
+            return AddCore(product);
+        }
+
+        /// <summary>Edits an existing product.</summary>
+        /// <param name="product">The product to update.</param>
+        /// <param name="message">Error message.</param>
+        /// <returns>The updated product.</returns>
+        /// <remarks>
+        /// Returns an error if product is null, invalid, product name
+        /// already exists or if the product cannot be found.
+        /// </remarks>
+        public Product Update ( Product product, out string message )
+        {
+            message = "";
+            //Check for null
+            if (product == null)
+            {
+                message = "Product cannot be null.";
+                return null;
+            };
+
+            //Validate product using IValidatableObject
+            //var error = product.Validate();
+            var errors = ObjectValidator.Validate(product);
+            if (errors.Count() > 0)
+            {
+                //Get first error
+                message = errors.ElementAt(0).ErrorMessage;
+                return null;
+            };
+
+            //Verify unique product
+            var existing = GetProductByNameCore(product.Name);
+            if (existing != null && existing.Id != product.Id)
+            {
+                message = "Product already exists.";
+                return null;
+            }
+            //Find existing
+            existing = existing ?? GetCore(product.Id);
+            if (existing == null)
+            {
+                message = "Product not found.";
+                return null;
+            };
+
+            return UpdateCore(product);
+        }
+
+        /// <summary>Gets all products.</summary>
+        /// <returns>The list of products.</returns>
+        public IEnumerable<Product> GetAll ()
+        {
+            return GetAllCore();
+        }
+
+        /// <summary>Removes a product.</summary>
+        /// <param name="id">The product ID.</param>
+        public void Remove ( int id )
+        {
+            //TODO: Return an error if id <= 0
+
+            if (id > 0)
+            {
+                RemoveCore(id);
+            };
+        }
+
+        protected abstract Product AddCore( Product product );
+        protected abstract IEnumerable<Product> GetAllCore();
+        protected abstract Product GetCore( int id );
+        protected abstract void RemoveCore( int id );
+        protected abstract Product UpdateCore( Product product );
+        protected abstract Product GetProductByNameCore( string name );
+
+        #region Private Members
+
+        private readonly List<Product> _products = new List<Product>();
+        private int _nextId = 1;
+
+        #endregion
+    }
+}
